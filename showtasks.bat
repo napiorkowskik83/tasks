afcfb7345e0ec658d7cfd1a16bbcd99c317f3484
napiorkowskik83 <napiorkowskik83@gmail.com>
2020-09-07 13:41:00 +0200

    Add classes from 22.2 submodule

--- showtasks.bat
+++ showtasks.bat
@@ -2,10 +2,10 @@ call runcrud.bat
 if "%ERRORLEVEL%" == "0" goto openchrome
 echo.
 echo There were errors while executing runcrud.bat
+goto end
 
 :openchrome
 start chrome http://localhost:8080/crud/v1/task/getTasks
-goto end
 
 :end
 echo.
