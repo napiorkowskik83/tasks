04eb2823ec641b02d6c8866deaaca1977a23dde4
Krzysztof N <64217195+napiorkowskik83@users.noreply.github.com>
2020-10-17 13:31:12 +0200

    Add CC email (#2)

--- src/main/java/com/crud/tasks/service/SimpleEmailService.java
+++ src/main/java/com/crud/tasks/service/SimpleEmailService.java
@@ -31,6 +31,9 @@ public class SimpleEmailService {
     private SimpleMailMessage createMailMessage(final Mail mail){
         SimpleMailMessage mailMessage = new SimpleMailMessage();
         mailMessage.setTo(mail.getMailTo());
+        if (mail.getToCc() != null && !"".equals(mail.getToCc())){
+            mailMessage.setCc(mail.getToCc());
+        }
         mailMessage.setSubject(mail.getSubject());
         mailMessage.setText(mail.getMessage());
         return mailMessage;
