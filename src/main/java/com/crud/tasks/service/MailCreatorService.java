d71737b70a705915d33b2193bf5ecd26d62f546a
napiorkowskik83 <napiorkowskik83@gmail.com>
2020-11-16 23:17:26 +0100

    Add modifications from 32.3 submodule

--- src/main/java/com/crud/tasks/service/MailCreatorService.java
+++ src/main/java/com/crud/tasks/service/MailCreatorService.java
@@ -7,6 +7,9 @@ import org.springframework.stereotype.Service;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.Context;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
 public class MailCreatorService {
 
@@ -17,16 +20,23 @@ public class MailCreatorService {
     @Qualifier("templateEngine")
     private TemplateEngine templateEngine;
 
-    public String buildTrelloCardEmail(String message){
+    public String buildTrelloCardEmail(String message) {
+
+        List<String> functionality = new ArrayList<>();
+        functionality.add("You can manage your tasks");
+        functionality.add("Provides connection with Trello Account");
+        functionality.add("Application allows sending tasks to Trello");
+
         Context context = new Context();
         context.setVariable("message", message);
         context.setVariable("tasks_url", "http://localhost:8888/tasks_frontend");
         context.setVariable("button", "visit website");
         context.setVariable("admin_name", adminConfig.getAdminName());
-        context.setVariable("goodbye_message","Best Regards,");
-        context.setVariable("company_name",adminConfig.getCompanyName());
-        context.setVariable("company_mail",adminConfig.getCompanyEmail());
-        context.setVariable("company_phone",adminConfig.getCompanyPhone());
+        context.setVariable("goodbye_message", "Best Regards,");
+        context.setVariable("show_button", false);
+        context.setVariable("is_friend", false);
+        context.setVariable("admin_config", adminConfig);
+        context.setVariable("application_functionality", functionality);
         return templateEngine.process("mail/created-trello-card-mail", context);
     }
 }
