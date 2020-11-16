3689a3f61e680d34317f207dea62aab1e9f00623
napiorkowskik83 <napiorkowskik83@gmail.com>
2020-11-16 16:50:34 +0100

    Add modifications from 32.2 submodule

--- src/main/java/com/crud/tasks/service/SimpleEmailService.java
+++ src/main/java/com/crud/tasks/service/SimpleEmailService.java
@@ -7,6 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.MailException;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.mail.javamail.MimeMessagePreparator;
 import org.springframework.stereotype.Service;
 
 
@@ -18,20 +20,32 @@ public class SimpleEmailService {
     @Autowired
     private JavaMailSender javaMailSender;
 
-    public void send(final Mail mail){
+    @Autowired
+    private MailCreatorService mailCreatorService;
+
+    public void send(final Mail mail) {
         LOGGER.info("Starting email preparation...");
-        try{
-            javaMailSender.send(createMailMessage(mail));
+        try {
+            javaMailSender.send(createMimeMessage(mail));
             LOGGER.info("Email has been sent.");
-        }catch(MailException e){
+        } catch (MailException e) {
             LOGGER.error("Failed to process email sending: ", e.getMessage(), e);
         }
     }
 
-    private SimpleMailMessage createMailMessage(final Mail mail){
+    private MimeMessagePreparator createMimeMessage(final Mail mail) {
+        return mimeMessage -> {
+            MimeMessageHelper messageHelper = new MimeMessageHelper(mimeMessage);
+            messageHelper.setTo(mail.getMailTo());
+            messageHelper.setSubject(mail.getSubject());
+            messageHelper.setText(mailCreatorService.buildTrelloCardEmail(mail.getMessage()), true);
+        };
+    }
+
+    private SimpleMailMessage createMailMessage(final Mail mail) {
         SimpleMailMessage mailMessage = new SimpleMailMessage();
         mailMessage.setTo(mail.getMailTo());
-        if (mail.getToCc() != null && !"".equals(mail.getToCc())){
+        if (mail.getToCc() != null && !"".equals(mail.getToCc())) {
             mailMessage.setCc(mail.getToCc());
         }
         mailMessage.setSubject(mail.getSubject());
